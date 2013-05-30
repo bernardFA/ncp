069b40320324b69c69d5756ae79554802b3f3a18
Bernard <blaurant@financeactive.com>
2013-05-30 14:47:04 +0200

    LiveCycle : add simulated state (and simplify names)

--- src/main/java/com/fa/insito/poc3/LiveCycleState.java
+++ src/main/java/com/fa/insito/poc3/LiveCycleState.java
@@ -8,22 +8,31 @@ public class LiveCycleState extends StateEngine {
 
     // states
     private static final String ALIVE = "Alive";
+    private static final String SIMULATED = "Simulated";
     private static final String DEAD = "Dead";
     private static final String HIBERNANT = "Hibernant";
 
     private static final String DESTROY = "Destroy";
-    private static final String RESUR = "Resur";
+    private static final String SIMULATE = "Simulate";
     private static final String HIBERN = "Hibern";
-    private static final String AWAKE = "Awake";
-    private static final String RESURTOHIBERN = "ResurToHibern";
+    private static final String GOLIVE = "Go live";
 
     LiveCycleState() {
         super(new State(ALIVE),
                 new Transition(new State(ALIVE), DESTROY, new State(DEAD)),
                 new Transition(new State(ALIVE), HIBERN, new State(HIBERNANT)),
-                new Transition(new State(HIBERNANT), AWAKE, new State(ALIVE)),
+                new Transition(new State(ALIVE), SIMULATE, new State(SIMULATED)),
+
+                new Transition(new State(SIMULATED), GOLIVE, new State(ALIVE)),
+                new Transition(new State(SIMULATED), HIBERN, new State(HIBERNANT)),
+                new Transition(new State(SIMULATED), DESTROY, new State(DEAD)),
+
+                new Transition(new State(HIBERNANT), GOLIVE, new State(ALIVE)),
                 new Transition(new State(HIBERNANT), DESTROY, new State(DEAD)),
-                new Transition(new State(DEAD), RESUR, new State(ALIVE)),
-                new Transition(new State(DEAD), RESURTOHIBERN, new State(HIBERNANT)));
+                new Transition(new State(HIBERNANT), SIMULATE, new State(SIMULATED)),
+
+                new Transition(new State(DEAD), GOLIVE, new State(ALIVE)),
+                new Transition(new State(DEAD), HIBERN, new State(HIBERNANT)),
+                new Transition(new State(DEAD), SIMULATE, new State(SIMULATED)));
     }
 }
